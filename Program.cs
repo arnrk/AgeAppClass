cc964385d709d3bb85ea333a91c215aa83e32c1e
Aaron Rock <aaronrk@mail.com>
2019-09-04 20:05:57 -0400

    Added User Input Age

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace AgeAppClass
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Age App 1.0");
+
+            Console.WriteLine("Please Enter Your Age When Ready: ");
+
+            Console.ReadLine();
+
         }
     }
 }
